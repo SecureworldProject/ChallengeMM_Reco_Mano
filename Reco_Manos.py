098d3e17bdfbcb1846f1f4763adc49b68137ee4c
IsabelHM1403 <72402308+IsabelHM1403@users.noreply.github.com>
2022-11-23 21:53:57 +0100

    Update Reco_Manos.py

--- Reco_Manos.py
+++ Reco_Manos.py
@@ -56,7 +56,7 @@ def executeChallenge():
         ventana= tk.Tk()
         ventana.geometry('450x100')
         tk.Label(ventana,text='Necesitas un escaner para comprobar la imagen', font = "Calibri 16").pack()
-        #Botón para cerrar ventana una vez que el usuario introdujo su nombre y apellidos
+        #BotÃ³n para cerrar ventana una vez que el usuario introdujo su nombre y apellidos
         button1 = tk.Button(ventana, text = "OK", command = cerrar).pack(side= tk.BOTTOM)
         ventana.mainloop()
         rmse=-1
@@ -69,14 +69,14 @@ def executeChallenge():
     
         ventana= tk.Tk()
         ventana.geometry('750x150')
-        tk.Label(ventana,text='Antes de pulsar OK crea una carpeta llamada:  MANO (si aún no la has creado) \n en la ruta '  + d_Path + ' \n y guarde en ella una imagen escaneada de la palma de la mano derecha\n ', font = "Calibri 16").pack()
-        #Botón para cerrar ventana una vez que el usuario introdujo su nombre y apellidos
+        tk.Label(ventana,text='Antes de pulsar OK crea una carpeta llamada:  MANO (si aÃºn no la has creado) \n en la ruta '  + d_Path + ' \n y guarde en ella una imagen escaneada de la palma de la mano derecha\n ', font = "Calibri 16").pack()
+        #BotÃ³n para cerrar ventana una vez que el usuario introdujo su nombre y apellidos
         button1 = tk.Button(ventana, text = "OK", command = cerrar).pack(side= tk.BOTTOM)
         ventana.mainloop()
 
         dataPath = d_Path + '/' + 'MANO' #Cambia a la ruta donde se almacene la imagen diaria
 
-        #comprobando que el directorio de la carpeta MANO no está vacio
+        #comprobando que el directorio de la carpeta MANO no estÃ¡ vacio
         direct = os.listdir(dataPath) 
         if len(direct) != 0: 
 
@@ -107,7 +107,7 @@ def executeChallenge():
                             ventana= tk.Tk()
                             ventana.geometry('450x100')
                             tk.Label(ventana,text='Entre una imagen de la mano derecha', font = "Calibri 16").pack()
-                            #Botón para cerrar ventana una vez que el usuario introdujo su nombre y apellidos
+                            #BotÃ³n para cerrar ventana una vez que el usuario introdujo su nombre y apellidos
                             button1 = tk.Button(ventana, text = "OK", command = cerrar).pack(side= tk.BOTTOM)
                             ventana.mainloop()
 
@@ -154,7 +154,7 @@ def executeChallenge():
                                 dis10= calcularDistancia(x10,y10,x0,y0)
                                 dis101= calcularDistancia(x101,y101,x0,y0)
                         
-                                # Dibujando los puntos de interés
+                                # Dibujando los puntos de interÃ©s
                                 cv2.circle(image, (x0, y0), 3,(255,0,0),3)
                                 cv2.circle(image, (x6, y6), 3,(255,0,0),3)
                                 cv2.circle(image, (x61, y61), 3,(255,0,0),3)
@@ -183,7 +183,7 @@ def executeChallenge():
                                         ,'Dist 2-0': [(resul[10])]}
 
                         
-                            # Creación DataFrame:
+                            # CreaciÃ³n DataFrame:
                             tab_result = pd.DataFrame(data)
                         
 
@@ -191,7 +191,7 @@ def executeChallenge():
                             rmse= sqrt((((tab_result.iloc[0, 1]-df.iloc[0, 1])**2)+((tab_result.iloc[0, 2]-df.iloc[0, 2])**2)+((tab_result.iloc[0, 3]-df.iloc[0, 3])**2)+((tab_result.iloc[0, 4]-df.iloc[0, 4])**2)+((tab_result.iloc[0, 5]-df.iloc[0, 5])**2)+((tab_result.iloc[0, 6]-df.iloc[0, 6])**2)+((tab_result.iloc[0, 7]-df.iloc[0, 7])**2)+((tab_result.iloc[0, 8]-df.iloc[0, 8])**2)+((tab_result.iloc[0, 9]-df.iloc[0, 9])**2)+((tab_result.iloc[0, 10]-df.iloc[0, 10])**2))/10)
                             
                             #Eliminando imagen de la carpeta MANO para que el usuario tenga que guardarla cada vez que se ejecute el challenge
-                            #remove(dataPath + "/" + fileName)
+                            remove(dataPath + "/" + fileName)
                 image = cv2.flip(image, 1)
                 cv2.imshow("Image",image)
                 cv2.waitKey(0)
@@ -204,7 +204,7 @@ def executeChallenge():
             ventana= tk.Tk()
             ventana.geometry('850x100')
             tk.Label(ventana,text='Debes guardar una imagen de la palma de la mano derecha escaneada en una carpeta llamada: \n MANO en la ruta '  + d_Path + ' ', font = "Calibri 16").pack()
-            #Botón para cerrar ventana una vez que el usuario introdujo su nombre y apellidos
+            #BotÃ³n para cerrar ventana una vez que el usuario introdujo su nombre y apellidos
             button1 = tk.Button(ventana, text = "OK", command = cerrar).pack(side= tk.BOTTOM)
             ventana.mainloop()
             rmse=-1
